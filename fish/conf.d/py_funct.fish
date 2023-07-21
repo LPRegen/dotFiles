ec2638f45799f7bedd1d8fe001f2f45bdc71b14e
LPRegen <manuel.escribano.051@gmail.com>
2023-07-21 12:44:10 +0200

    refactor: modify vEnv funct - extend functionality to take options for arguments

--- fish/conf.d/py_funct.fish
+++ fish/conf.d/py_funct.fish
@@ -1,14 +1,23 @@
 function vEnv
-  switch $argv
-    case create
-      python3 -m venv .venv
-    case activate
-      source .venv/bin/activate.fish
-    case installAll
-      python3 -m pip install -r requirements.txt
-    case install
-      python3 -m pip install
-    case ''
-      echo 'Please give me a command! -> create | activate | install | installAll'
+  argparse h/help c/create a/activate I/Install i/install -- $argv
+  or return
+  if set -ql _flag_help
+    echo "vEnv [-h|--help] [-o/option] [ARGUMENTS ...]
+    -c/create              -> Creates a virtual environment.
+    -a/activate            -> Activates virtual environment.
+    -I/Install             -> Install ALL the requirements.
+    -i/install [ARGUMENTS] -> Install dependency/es with PIP."
+    return 0
+  end
+  if set -ql _flag_activate
+    echo 'Activating virtual environment..'
+    source .venv/bin/activate.fish
+  else if set -ql _flag_create
+    echo 'Creating virtual environment'
+    python3 -m venv .venv
+  else if set -ql _flag_Install
+    python3 -m pip install -r requirements.txt
+  else if set -ql _flag_install
+    python3 -m pip install $argv
   end
 end
